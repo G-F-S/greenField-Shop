5b0b69189d0d2434522ad2514cec8fb3a4eed50e
Fatma Miled <you@example.com>
2022-12-06 07:13:42 +0100

    done

--- green-field/src/App.jsx
+++ green-field/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
-
+import Footer from './components/Footer.jsx';
+import NavBar from './components/NavBar.jsx';
+import {Routes,Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
       
-    </div>
+   
+   <NavBar/>
+<Routes>
+  <Route>
+<Route path='/fot' element={<Footer/>}/>
+<Route path='/fot' element={<Footer/>}/>
+<Route path='/fot' element={<Footer/>}/>
+<Route path='/fot' element={<Footer/>}/>
+<Route path='/fot' element={<Footer/>}/>
+  </Route>
+</Routes>
+    < Footer/>
+        </div>
   );
 }
 
